1e92255d890ddaa0b0eb0cb49e209b63da339ab7
Fitsum Seyoum <fitsum.s@yahoo.com>
2020-04-26 16:44:02 -0400

    Binary Search

--- arrayCharactersCount/src/com/fitsum/Main.java
+++ arrayCharactersCount/src/com/fitsum/Main.java
@@ -5,52 +5,31 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-
-        //generate 100 lowercase letters randomly and assign them to an array of characters
-        // this means 97  to 122
-        char[] charElements = new char[100];
-
-        // array to hold each characters
-        int[] counts = new int[26];
+             int value =  printMax(new int[]{3,5,9});
+        System.out.println(value);
 
 
-        for (int i = 0; i < 100 ; i++) {
-
-            int aschiValues = (int)(97 + (25 * Math.random()));
-            charElements[i] = (char)(aschiValues);
-           // System.out.println(charElements[i]);
-        }
-
-
-        //Here is where counting of the individual characters is happening, please take a look
-        //very interesting concept
-        for (int i = 0; i < charElements.length; i++) {
-
-            int val = charElements[i] -  'a';
-            counts[val] = counts[val] + 1;
 
         }
+            public static int printMax(int...numbers) {
 
-        System.out.println("***************************");
-        for (int i = 0; i < charElements.length; i++) {
+                if (numbers.length == 0) {
+                    System.out.println("no args");
 
-            if((i + 1) % 20 == 0) {
-                System.out.println(charElements[i] + " ");
-            } else {
-                System.out.print(charElements[i] + " ");
+                }
+                int max = numbers[0];
+                for (int i = 0; i < numbers.length; i++) {
+                    if(numbers[i] > max) {
+                        max = numbers[i];
+                    }
+                }
+
+
+                return max;
             }
-        }
-
-        System.out.println("****************************");
-        for (int i = 0; i < counts.length; i++) {
-            if ((i + 1) == 10) {
-                System.out.println(counts[i] + " " + (char)(i + 'a'));
-
-            } else {
-                System.out.print(counts[i] + " "+ (char)(i + 'a') + " ");
-            }
-        }
 
 
-    }
+
+
+
 }
